50756370d3fd267a311e5815515f09caf311d8de
manager <manager@163.com>
2019-06-05 15:11:58 +0800

    flask_hello world

--- login.py
+++ login.py
@@ -1,3 +1,16 @@
 a = 1
 b = 2
 c = 3 
+from flask import Flask
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+
+@app.route("/")
+def index():
+    return 'hello world'
+
+if __name__ == "__main__":
+    app.run()
